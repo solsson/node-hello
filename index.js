f73d9ca0e5e4ad5d0cc3ceb180a8b8b85b654edb
Staffan Olsson <staffan@repos.se>
2016-04-17 17:03:51 +0200

    New tag names

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello. You probably want to set the env for docker solsson/node:run-zip now to get a real app.');
+  res.send('Hello. You probably want to set the env for docker solsson/node:run now to get a real app.');
 });
 
 app.listen(80, function () {
